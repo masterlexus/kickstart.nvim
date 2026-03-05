41dcfba1e9b24480dd93f5fd6860744c0d89efbb
Alex orri <alexorri2003@gmail.com>
2026-03-05 09:18:06 +0100

    remove comments

--- init.lua
+++ init.lua
@@ -14,10 +14,6 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 --  Commented this cuz I want my shit stored in "+ register
 --  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
